b29386391d708be76cf19ba35c1193f4e2d80941
Aleksei Skokov <a@1x.ee>
2020-01-31 15:39:32 +0200

    Initial Commit: Added Boilerplate for the project Changed name of Main class to EcommerceSolution.java

---
+++ src/main/java/com/onex/ecommercesolution/EcommerceSolution.java
@@ -0,0 +1,13 @@
+package com.onex.ecommercesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSolution {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSolution.class, args);
+	}
+
+}
